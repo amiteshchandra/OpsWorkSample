dafefadf6f73503cb9b13f8659b49a6eabddb0fa
Amitesh <AmiteshJ@>
2016-12-07 16:13:32 -0500

    Update the file for Apache

--- cookbook/recipes/default.rb
+++ cookbook/recipes/default.rb
@@ -2,13 +2,20 @@ package 'httpd' do
   action :install
 end
 
-service 'httpd' do
-  action :start
-end
-
 directory "/home/ec2-user/amitesh" do
   mode 0755
   owner 'root'
   group 'root'
   action :create
 end
+
+cookbook_file "/var/www/index.html" do
+  source "index.html"
+  mode 0644
+  action :create_if_missing
+end
+
+service 'httpd' do
+  action :start
+end
+
